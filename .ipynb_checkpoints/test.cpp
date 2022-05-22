ef71e99d1d15a5df0adb8254b3cbd3231edaa3a7
TanishkaRajput99 <76390822+TanishkaRajput99@users.noreply.github.com>
2022-05-22 13:17:59 +0530

    sdl;kfja

---
+++ .ipynb_checkpoints/test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i,j;
+    cout<<"tested by sachin asdfsfasndasefansdlfkasld\n";
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            cout<<"*";
+        }
+        cout<<"\n";
+
+    }
+}
